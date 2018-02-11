815fc4132de238f8821631f78d0f6a10df3b98f2
Sylvain MARTIN <sylvain.martin@e-doceo.net>
2018-02-12 00:06:16 +0100

    Possibilité de changer les paramètres pour générer des canvas d'exemple.

--- src/script.js
+++ src/script.js
@@ -3,6 +3,75 @@ import 'bootstrap';
 import '../scss/_custom.scss';
 import dat from '../node_modules/dat.gui/build/dat.gui.js'
 
+
+let BookMark = {};
+
+let Params = function () {
+	this.canvas1height = 600;
+	this.canvas1width = 300;
+	this.canvas1color = '#000';
+	this.canvas1numberOfTriangles = 3;
+
+	this.canvas2height = 600;
+	this.canvas2width = 300;
+	this.canvas2color = '#000';
+	this.canvas2numberOfTriangles = 3;
+
+	this.canvas3height = 600;
+	this.canvas3width = 300;
+	this.canvas3color = '#000';
+	this.canvas3numberOfTriangles = 3;
+};
+
+BookMark.init = function () {
+
+	let params = new Params();
+	let gui = new dat.GUI();
+
+	let numberOflayer = 3;
+
+	for (let i = 1; i <= numberOflayer; i++) {
+
+		let el_canvas = this.createCanvas('canva-' + i, 'zone-' + i, params);
+		let elctx = el_canvas.getContext('2d');
+
+		let folder = gui.addFolder('Example ' + i);
+		let numberOfTriangles = 3;
+
+		let redrawNumberOfTriangles = function (el) {
+			BookMark.clearCanvas(elctx);
+			numberOfTriangles = el;
+			BookMark.drawTriangles(el_canvas, elctx, el);
+		};
+
+		let redrawHeight = function (el) {
+			BookMark.clearCanvas(elctx);
+			el_canvas.height = el;
+			BookMark.drawTriangles(el_canvas, elctx, numberOfTriangles);
+		};
+
+		let redrawWidth = function (el) {
+			BookMark.clearCanvas(elctx);
+			el_canvas.width = el;
+			BookMark.drawTriangles(el_canvas, elctx, numberOfTriangles);
+		};
+
+		let redrawColor = function (el) {
+			BookMark.clearCanvas(elctx);
+			el_canvas.style.backgroundColor = el;
+			BookMark.drawTriangles(el_canvas, elctx, numberOfTriangles);
+		};
+
+		folder.add(params, 'canvas' + i + 'numberOfTriangles', 3, 15, 1).onFinishChange(redrawNumberOfTriangles);
+		folder.add(params, 'canvas' + i + 'height', 100, 1000, 100).onFinishChange(redrawHeight);
+		folder.add(params, 'canvas' + i + 'width', 100, 300, 100).onFinishChange(redrawWidth);
+		folder.addColor(params, 'canvas' + i + 'color').onFinishChange(redrawColor);
+
+		BookMark.drawTriangles(el_canvas, elctx, numberOfTriangles);
+
+	}
+};
+
 /**
  * Dessine un canvas.
  * @param elementId
@@ -10,45 +79,42 @@ import dat from '../node_modules/dat.gui/build/dat.gui.js'
  * @param params
  * @returns {Element}
  */
-function createCanvas(elementId, zoneId, params) {
+BookMark.createCanvas = function (elementId, zoneId, params) {
 
 	let canvas = document.createElement('canvas');
 
 	canvas.id = elementId;
-	canvas.width = 100;
-	canvas.height = 100;
-	canvas.style.backgroundColor = '#333333';
+	canvas.width = params.canvas1width;
+	canvas.height = params.canvas1height;
+	canvas.style.backgroundColor = params.canvas1color;
 
 	let zone = document.getElementById(zoneId);
 	zone.appendChild(canvas);
 
-	clearCanvas(canvas.getContext('2d'));
+	this.clearCanvas(canvas.getContext('2d'));
 
 	return canvas;
-}
+};
 
 /**
  * Efface un canvas.
  * @param ctx
  */
-function clearCanvas(ctx) {
-	/* because the GUI will re-draw the canvas, we need to be able to clear
-	   it before drawing things another time. This function clears the
-	   canvas to white by finding out how big it is and drawing a white
-	   rectangle. */
+BookMark.clearCanvas = function (ctx) {
+
 	let canvas = ctx.canvas;
 	let w = canvas.clientWidth;
 	let h = canvas.clientHeight;
 	ctx.clearRect(0, 0, w, h);
-}
+};
 
 /**
- *
+ * Dessine les couples de triangles.
  * @param el_canvas
  * @param elctx
  * @param numberOfTriangles
  */
-function drawTriangles(el_canvas, elctx, numberOfTriangles) {
+BookMark.drawTriangles = function (el_canvas, elctx, numberOfTriangles) {
 
 	let equi = false;
 
@@ -56,46 +122,46 @@ function drawTriangles(el_canvas, elctx, numberOfTriangles) {
 
 		if (el_canvas) {
 
-			let _w = el_canvas.width;
-			let _h = el_canvas.height;
-			let _a = _w / 2;
-			let _b;
+			let _width = el_canvas.width;
+			let _height = el_canvas.height;
+			let _half_width = _width / 2;
+			let _triangle_height;
 
 			// Cas spécifique, on veut un triangle equilatéral, on le calcul en fonction de la largeur du canvas.
 			if (equi === true) {
-				_b = Math.sqrt((Math.pow(_w, 2) + Math.pow((_a / 2), 2)));
+				_triangle_height = Math.sqrt((Math.pow(_width, 2) + Math.pow((_half_width / 2), 2)));
 
 				if (j === 1) {
-					el_canvas.height = _b * numberOfTriangles * 2;
+					el_canvas.height = _triangle_height * numberOfTriangles * 2;
 				}
 			} else {
-				_b = _h / (numberOfTriangles * 2);
+				_triangle_height = _height / (numberOfTriangles * 2);
 			}
 
 			if (elctx) {
 
 				elctx.lineWidth = 1;
 
-				let coef = 2 * (_b * j - _b);
-				let coef2 = coef + 2 * _b;
-				let coef3 = (coef2 - _b) / j * j;
+				let first_coef = 2 * (_triangle_height * j - _triangle_height);
+				let second_coef = first_coef + 2 * _triangle_height;
+				let third_coef = (second_coef - _triangle_height) / j * j;
 
 				// Dessine une paire de triangle.
 				for (let k = 1; k <= 2; k++) {
 					elctx.beginPath();
 
-					elctx.moveTo(_a, coef3);
+					elctx.moveTo(_half_width, third_coef);
 
 					let red = _.random(0, 255);
 					let green = _.random(0, 255);
 					let blue = _.random(0, 255);
 
 					if (k % 2 === 1) {
-						elctx.lineTo(_w, coef);
-						elctx.lineTo(0, coef);
+						elctx.lineTo(_width, first_coef);
+						elctx.lineTo(0, first_coef);
 					} else {
-						elctx.lineTo(_w, coef2);
-						elctx.lineTo(0, coef2);
+						elctx.lineTo(_width, second_coef);
+						elctx.lineTo(0, second_coef);
 					}
 
 					elctx.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
@@ -106,71 +172,8 @@ function drawTriangles(el_canvas, elctx, numberOfTriangles) {
 			}
 		}
 	}
-
-}
-
-let Params = function () {
-	this.canvas1height = 100;
-	this.canvas1width = 100;
-	this.canvas1color = '#000';
-	this.canvas1numberOfTriangles = 3;
-
-	this.canvas2height = 100;
-	this.canvas2width = 100;
-	this.canvas2color = '#000';
-	this.canvas2numberOfTriangles = 3;
-
-	this.canvas3height = 100;
-	this.canvas3width = 100;
-	this.canvas3color = '#000';
-	this.canvas3numberOfTriangles = 3;
 };
 
 window.onload = function () {
-
-	let params = new Params();
-	let gui = new dat.GUI();
-
-	let numberOflayer = 3;
-
-	for (let i = 1; i <= numberOflayer; i++) {
-
-		let el_canvas = createCanvas('canva-' + i, 'zone-' + i, params);
-		let elctx = el_canvas.getContext('2d');
-
-		let folder = gui.addFolder('Example ' + i);
-		let numberOfTriangles = 3;
-
-		let redrawNumberOfTriangles = function (el) {
-			clearCanvas(elctx);
-			numberOfTriangles = el;
-			drawTriangles(el_canvas, elctx, el);
-		};
-
-		let redrawHeight = function (el) {
-			clearCanvas(elctx);
-			el_canvas.height = el;
-			drawTriangles(el_canvas, elctx, numberOfTriangles);
-		};
-
-		let redrawWidth = function (el) {
-			clearCanvas(elctx);
-			el_canvas.width = el;
-			drawTriangles(el_canvas, elctx, numberOfTriangles);
-		};
-
-		let redrawColor = function (el) {
-			clearCanvas(elctx);
-			el_canvas.style.backgroundColor = el;
-			drawTriangles(el_canvas, elctx, numberOfTriangles);
-		};
-
-		folder.add(params, 'canvas' + i + 'numberOfTriangles', 3, 15, 1).onFinishChange(redrawNumberOfTriangles);
-		folder.add(params, 'canvas' + i + 'height', 100, 1000, 100).onFinishChange(redrawHeight);
-		folder.add(params, 'canvas' + i + 'width', 100, 300, 100).onFinishChange(redrawWidth);
-		folder.addColor(params, 'canvas' + i + 'color').onFinishChange(redrawColor);
-
-		drawTriangles(el_canvas, elctx, numberOfTriangles);
-
-	}
+	BookMark.init();
 };
