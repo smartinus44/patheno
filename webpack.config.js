b166dba5648050943832405a35a8e6b3df86ea60
Sylvain MARTIN <sylvain.martin@e-doceo.net>
2018-02-28 01:08:16 +0100

    Notation en classe es6.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
 	entry: './src/script.js',
@@ -8,26 +9,47 @@ module.exports = {
 	},
 	watch: true,
 	module: {
-		rules: [{
-
-			test: /\.(scss)$/,
-			use: [{
-				loader: 'style-loader', // inject CSS to page
-			}, {
-				loader: 'css-loader', // translates CSS into CommonJS modules
-			}, {
-				loader: 'postcss-loader', // Run post css actions
-				options: {
-					plugins: function () { // post css plugins, can be exported to postcss.config.js
-						return [
-							require('precss'),
-							require('autoprefixer')
-						];
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
 					}
 				}
-			}, {
-				loader: 'sass-loader' // compiles Sass to CSS
-			}]
-		},]
-	}
+			},
+			{
+				test: /\.(scss)$/,
+				use: [
+					{
+						loader: 'style-loader', // inject CSS to page
+					}, {
+						loader: 'css-loader', // translates CSS into CommonJS modules
+					}, {
+						loader: 'postcss-loader', // Run post css actions
+						options: {
+							plugins: function () { // post css plugins, can be exported to postcss.config.js
+								return [
+									require('precss'),
+									require('autoprefixer')
+								];
+							}
+						}
+					}, {
+						loader: 'sass-loader' // compiles Sass to CSS
+					}
+				]
+			},]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			test: /\.js($|\?)/i,
+			sourceMap: true,
+			uglifyOptions: {
+				compress: true
+			}
+		}),
+	]
 };
