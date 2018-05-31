c6b2d102cc9f8442c3d1fa084de193deb71bfd43
Sylvain MARTIN <sylvain.martin-44@laposte.net>
2018-06-01 00:15:39 +0200

    Server.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+//---ExpressJS
+console.log('Initializing Express...')const express = require('express');
 const app = express();
 app.disable('x-powered-by');
 
@@ -29,4 +30,10 @@ app.get('/patterns/:id', function (req, res, next) {
 
 });
 
-app.listen(3000, () => console.log('Partheno server listening on port 3000!'));
+//app.listen(3000, () => console.log('Partheno server listening on port 3000!'));
+
+
+//---Start listening
+var port = 80;
+app.listen(port);
+console.log('Partheno server  started on port: '+port);
