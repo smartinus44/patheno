06573988a07d2ff8c09a3c4de806484f5eef4a30
Sylvain Martin <sylv1.martin44@gmail.com>
2023-07-11 12:00:55 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'eslint-loader',
-        options: {
-          eslintPath: eslintrcFile,
-          cache: true,
-        },
       },
       {
         test: /\.js$/,
